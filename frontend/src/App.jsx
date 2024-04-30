0975a1ba51cf8fd83d17d04093a2c2d3fa293a90
utkarshmalik07 <utkarsh.malik@shyftlabs.io>
2024-04-30 16:45:37 +0530

    frontend-half

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import {BrowserRouter , Route, Routes} from "react-router-dom"
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello Paytm</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>}/>
+            <Route path="/signin" element={<Signin/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/sendmoney" element={<SendMoney/>}/>
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
